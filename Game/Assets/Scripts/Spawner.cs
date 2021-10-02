684984bcaecaf07c61fabdeda3555aed83b5b946
Оборожный Данил <denob311302@gmail.com>
2021-10-02 20:32:45 +0300

    Spawn
    Spawn enemy
    New room

--- Game/Assets/Scripts/Spawner.cs
+++ Game/Assets/Scripts/Spawner.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject[] EnemyList;
+
+    public void StartSpawn(int countEnemy, int[] typeEnemy)
     {
-        
+        print("start");
+        //Spawn(0.2f);
+        //TestCoroutine();
+        InvokeRepeating("Spawn", 0f, 0.3F);
+        StartCoroutine(Stop(2f));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator TestCoroutine()
     {
-        
+        while (true)
+        {
+            yield return null;
+            Debug.Log(Time.deltaTime);
+        }
+    }
+
+    void Spawn()
+    {
+        System.Random rnd = new System.Random();
+        int x = rnd.Next(-13, 13);
+        int y = rnd.Next(-10, 8);
+        Instantiate(EnemyList[0], new Vector3(gameObject.transform.position.x + x, gameObject.transform.position.y + y, -1f), Quaternion.identity);
+    }
+    IEnumerator Stop(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        CancelInvoke();
     }
 }
