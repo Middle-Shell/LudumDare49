7e6a5b0e080928bf524524b8bd7ed985f22b1b85
Оборожный Данил <denob311302@gmail.com>
2021-10-04 01:47:15 +0300

    ReStart

---
+++ Game/Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameObject hitEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //print(collision.transform.name);
+        GameObject effect =  Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        if(collision.transform.tag == "enemy")
+        {
+            collision.gameObject.GetComponent<Enemy>().Damage(2f);
+        }
+        if (collision.transform.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerController>().Damage(2f);
+        }
+        Destroy(gameObject);
+    }
+}
